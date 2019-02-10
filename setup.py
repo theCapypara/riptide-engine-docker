4822fb3acdcb73b42d5608a2755538e37f01eca5
Parakoopa <parakoopa@live.de>
2019-02-10 13:12:12 +0100

    Splits riptide into -> riptide_cli, riptide_lib, ripitde_engine_docker, riptide_db_mysql

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='riptide_engine_docker',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # TODO
+    ],
+    entry_points='''
+        [riptide.engine]
+        docker=riptide_engine_docker.engine:DockerEngine
+    ''',
+)
