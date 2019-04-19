8d3c16230926e89d4cb41e1c545aa8d3e2d2e641
Parakoopa <parakoopa@live.de>
2019-04-19 21:36:26 +0200

    Preperations for first release

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 
+# README read-in
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+# END README read-in
+
 setup(
     name='riptide_engine_docker',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
-    description='TODO',  # TODO
-    long_description='TODO - Project will be available starting May/June',  # TODO
+    description='Tool to manage development environments for web applications using containers - Docker Implementation',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/Parakoopa/riptide-engine-docker/',
     install_requires=[
-        'riptide_lib == 0.1',
+        'riptide_lib >= 0.1, < 0.2',
         'docker >= 3.5'
     ],
-    # TODO
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
