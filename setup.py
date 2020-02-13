ffc268aebce868f4326523b54a863a94ddea7d23
Marco Köpcke <marco.koepcke@tudock.de>
2020-02-13 14:16:43 +0100

    Version bump to 0.5.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='riptide-engine_docker',
-    version='0.5.0',
+    version='0.5.1',
     packages=find_packages(),
     package_data={'riptide_engine_docker': ['assets/*']},
     description='Tool to manage development environments for web applications using containers - Docker Implementation',
