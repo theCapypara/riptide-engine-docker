6b09d43a9d5297eef83acf13461265e3fb1d94c7
Marco Köpcke <marco.koepcke@tudock.de>
2022-07-27 11:39:56 +0200

    Build test images with BuildKit

--- run_tests.sh
+++ run_tests.sh
@@ -23,7 +23,7 @@ mkdir -p /tmp/riptide-docker-tox
 curl "https://raw.githubusercontent.com/theCapypara/riptide-lib/master/test_assets/riptide-docker-tox/Dockerfile" > /tmp/riptide-docker-tox/Dockerfile
 curl "https://raw.githubusercontent.com/theCapypara/riptide-lib/master/test_assets/riptide-docker-tox/entrypoint.sh" > /tmp/riptide-docker-tox/entrypoint.sh
 chmod +x /tmp/riptide-docker-tox/entrypoint.sh
-docker build -t riptide_docker_tox /tmp/riptide-docker-tox
+docker buildx build -t riptide_docker_tox /tmp/riptide-docker-tox
 
 
 # 2. Run the image...
