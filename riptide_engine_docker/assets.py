01c32ecff21e3f520f76f7304e8daebd29c8fb54
Julian Liebig <julian.liebig@tudock.de>
2023-09-01 14:28:00 +0200

    Replace pkg_resources with importlib

--- riptide_engine_docker/assets.py
+++ riptide_engine_docker/assets.py
@@ -1,5 +1,13 @@
-import pkg_resources
+import atexit
+import importlib.resources
+
+from contextlib import ExitStack
 
 
 def riptide_engine_docker_assets_dir():
-    return pkg_resources.resource_filename(__name__, 'assets')
+    file_manager = ExitStack()
+    atexit.register(file_manager.close)
+    package_name = __name__.split(".")[0]
+    ref = importlib.resources.files(package_name) / 'assets'
+    path = file_manager.enter_context(importlib.resources.as_file(ref))
+    return path
